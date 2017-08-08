09a38649b515432a60643d45ba4ec0ada81babb3
hz-heng <hz_heng@qq.com>
2017-08-08 11:43:58 +0800

    Update settings.py

--- newscrawl/settings.py
+++ newscrawl/settings.py
@@ -53,7 +53,7 @@ ROBOTSTXT_OBEY = False
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-    'scrapy.contrib,downloadermiddleware.useragent.UserAgentMiddleware': None,
+    'scrapy.downloadermiddleware.useragent.UserAgentMiddleware': None,
     'newscrawl.middlewares.RandomUserAgentMiddleware': 400,
 }
 
